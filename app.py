91640122fc38736366c112998d3714fe031d4874
28Nikita28 <nkostrikin432@gmail.com>
2025-03-17 22:25:17 +0300

    Фикс

--- app.py
+++ app.py
@@ -115,7 +115,8 @@ def _build_cors_preflight_response():
 def _corsify_actual_response(response):
     response.headers.add("Access-Control-Allow-Origin", "https://hdghs.onrender.com")
     response.headers.add("Access-Control-Allow-Credentials", "true")
-    response.headers.add("Access-Control-Allow-Headers", "Content-Type")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
     return response
 
 asgi_app = WsgiToAsgi(app)
